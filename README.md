0c17359bd21f1fa3750d5e9d4f394556972648ae
Philip Alegret <philip.alegret@gmail.com>
2020-05-24 20:17:55 +0000

    Initial files from Scrimba and changes from '3. Add Base Styles to Our App'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-Ready--to--Code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/palegret/scrimba-build-react-movie-search-app) 
 
 # scrimba-build-react-movie-search-app
-From Scrimba's free 'Build a React Movie Search App' tutorial
+From Scrimba's free 'Build a React Movie Search App' tutorial.
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import config from './config';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
