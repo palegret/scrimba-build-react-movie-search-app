0ff03b15406aff33e4d292894f82f92dcd4f7ab9
Philip Alegret <philip.alegret@gmail.com>
2020-05-25 17:53:25 +0000

    Changes related to '8. Display Movie Information'

--- src/MovieSearch.js
+++ src/MovieSearch.js
@@ -24,13 +24,14 @@ const handleError = (errorMessage, errorData) => {
 // =============================================================================
 
 export default function MovieSearch() {
+  const { apiUrl, apiKey, moviePosterUrl } = config;  
+
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const { apiUrl, apiKey } = config;  
     const url = `${apiUrl}/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
@@ -47,17 +48,35 @@ export default function MovieSearch() {
   };
 
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label htmlFor="query" className="label">Movie Name</label>
-      <input 
-        type="text" 
-        className="input" 
-        name="query" 
-        placeholder="Enter movie title, e.g., 'Jurassic Park'" 
-        value={query} 
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button type="submit" className="button">Search</button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label htmlFor="query" className="label">Movie Name</label>
+        <input 
+          type="text" 
+          className="input" 
+          name="query" 
+          placeholder="Enter movie title, e.g., 'Jurassic Park'" 
+          value={query} 
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">Search</button>
+      </form>
+      <div className="card-list">
+          {movies.filter(movie => movie.poster_path).map(movie => (
+            <div className="card" key={movie.id}>
+              <img className="card--image"
+                src={`${moviePosterUrl}/${movie.poster_path}`}
+                alt={`Movie poster for '${movie.title}'`}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                <p><small>RATING: {movie.vote_average}</small></p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   )
 }
