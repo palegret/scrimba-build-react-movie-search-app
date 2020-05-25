5d20dea99cba9881742a1c7db0fc588a546e9816
Philip Alegret <philip.alegret@gmail.com>
2020-05-25 17:12:14 +0000

    Changes related to '7. Manage State with React useState Hook'

--- src/MovieSearch.js
+++ src/MovieSearch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from 'react';
 
 import config from './config';
 
@@ -8,33 +8,55 @@ const handleError = (errorMessage, errorData) => {
   throw new Error(message);
 };
 
-const searchMovies = async (e) => {
-  e.preventDefault();
-
-  const query = "Jurassic Park";
-  
-  const { apiUrl, apiKey } = config;  
-  const url = `${apiUrl}/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
-
-  try {
-    const response = await fetch(url);
-    const data  = await response.json();    
-
-    if (!response.ok)
-      handleError('Unable to search for movies', data);
-
-    console.dir(data.results);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// =============================================================================
+// React useState hook
+// https://reactjs.org/docs/hooks-state.html
+//
+// Hooks "...are a new addition in React 16.8. They let you use state and other 
+// React features without writing a class."
+// 
+// Usage:
+// const [query, setQuery] = useState('');
+// 
+// Breakdown:
+// - useState(''): passes in default value for `query`, returns array
+// - [query, setQuery]: destructured state variable and updater function
+// =============================================================================
 
 export default function MovieSearch() {
+  const [query, setQuery] = useState('');
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const { apiUrl, apiKey } = config;  
+    const url = `${apiUrl}/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const response = await fetch(url);
+      const data  = await response.json();    
+
+      if (!response.ok)
+        handleError('Unable to search for movies', data);
+
+      console.dir(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-  <form className="form" onSubmit={searchMovies}>
-    <label htmlFor="query" className="label">Movie Name</label>
-    <input type="text" className="input" name="query" placeholder="Enter movie title, e.g., 'Jurassic Park'" />
-    <button type="submit" className="button">Search</button>
-  </form>
+    <form className="form" onSubmit={searchMovies}>
+      <label htmlFor="query" className="label">Movie Name</label>
+      <input 
+        type="text" 
+        className="input" 
+        name="query" 
+        placeholder="Enter movie title, e.g., 'Jurassic Park'" 
+        value={query} 
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit" className="button">Search</button>
+    </form>
   )
 }
